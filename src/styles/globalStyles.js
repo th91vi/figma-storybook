ad40d576f4809397dead1e41864464b8f3040fc3
Thiago Vieira <th91vi@gmail.com>
2021-11-26 15:49:12 -0300

    feat: added font files and configs

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,8 +1,36 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Poppins Light'),
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),        
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Poppins Regular'),
+        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'), 
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    src: local('Poppins Semibold'),
+        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),        
+  }
+
   body, html, * {
     margin: 0;
     padding: 0
   } 
+
+  body {
+    font-family: 'Poppins', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
 `;
