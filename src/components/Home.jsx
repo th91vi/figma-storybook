4163ff2c9f310f267092236e2134e4cd7d4a532a
Thiago Vieira <th91vi@gmail.com>
2021-11-26 15:36:48 -0300

    feat: added basic layout to Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import * as S from "./Home.styles";
+
+export const Home = () => {
+  return (
+    <S.Layout>
+      <S.Content>Teste</S.Content>
+    </S.Layout>
+  );
+};
---
+++ src/components/Home.styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #4c73f5;
+`;
+
+export const Content = styled.main`
+  width: 800px;
+  min-height: 500px;
+  padding: 1rem;
+  border-radius: 5px;
+  background-color: white;
+`;
